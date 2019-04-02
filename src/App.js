4da9d158fc4f829f06848152685918629e63d01d
jeffau13 <jeffau13@gmail.com>
2019-04-02 19:53:26 -0400

    used render props to create a toggle component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Toggle from './ToggleRPC';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Toggle>
+            {({ on, toggle }) => (
+              <div>
+                {on && <h1>Show me</h1>}
+                <button onClick={toggle}>Show / Hide</button>
+              </div>
+            )}
+          </Toggle>
         </header>
       </div>
     );
