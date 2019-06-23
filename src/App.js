fc41b646605e166f31052773061c428549b09221
jeffau13 <jeffau13@gmail.com>
2019-06-23 16:10:03 -0400

    created basic modal component

--- src/App.js
+++ src/App.js
@@ -3,22 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 import Toggle from './ToggleRPC';
 import Portal from './Portal';
+import Modal from './Modal';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Toggle>
-            {({ on, toggle }) => (
-              <Fragment>
-                {on && <h1>Show me</h1>}
-                <button onClick={toggle}>Show / Hide</button>
-                <Portal>{on && <h1>Hi I'm from the portal</h1>}</Portal>
-              </Fragment>
-            )}
-          </Toggle>
-        </header>
+        <Toggle>
+          {({ on, toggle }) => (
+            <Fragment>
+              <button onClick={toggle}>Login</button>
+              <Modal on={on} toggle={toggle}>
+                <h1>Modal is opened</h1>
+              </Modal>
+            </Fragment>
+          )}
+        </Toggle>
       </div>
     );
   }
